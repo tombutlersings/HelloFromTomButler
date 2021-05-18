cb74ca4ffae8681d8ffc7e28545720c7ff970495
pHr01d <pro20006@byui.edu>
2021-05-17 19:25:37 -0500

    Added a silly comment

--- src/butler/Main.java
+++ src/butler/Main.java
@@ -6,5 +6,6 @@ public class Main {
 	// write your code here
         System.out.println("Hello Tom, my name is Caleb.");
         System.out.println("Hi Tom! Keep playing with Zebras");
+        System.out.println("Zebras are cool and all ...but Tigers are cool, too.  Just sayin'");
     }
 }
